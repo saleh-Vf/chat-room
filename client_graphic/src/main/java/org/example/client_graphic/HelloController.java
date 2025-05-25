fbc4ff00b203b09c7cb3a4bcbf8a39fb526157d2
saleh-Vf <saleh.git.22@gmail.com>
2025-05-25 17:32:57 +0330

    inital controller

--- client_graphic/src/main/java/org/example/client_graphic/HelloController.java
+++ client_graphic/src/main/java/org/example/client_graphic/HelloController.java
@@ -1,14 +1,36 @@
 package org.example.client_graphic;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField input;
+
+    @FXML
+    private ScrollPane scrollPane;
+
+    @FXML
+    void sendMessage(ActionEvent event) {
+
     }
+
+    @FXML
+    void initialize() {
+        assert input != null : "fx:id=\"input\" was not injected: check your FXML file 'chat_page.fxml'.";
+        assert scrollPane != null : "fx:id=\"scrollPane\" was not injected: check your FXML file 'chat_page.fxml'.";
+
+    }
+
 }
---
+++ client_graphic/src/main/java/org/example/client_graphic/messageController.java
@@ -0,0 +1,20 @@
+package org.example.client_graphic;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class messageController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
